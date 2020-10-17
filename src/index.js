960d839c9907f2f321b923b8800e08c1e48f6f03
Zykava Alena <zykava.alena@gmail.com>
2020-10-17 23:55:13 +0300

    :fix: function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    if (n >= 0) {
+        let result = n.toString().split('');
+  return result.reverse().join('');
+    } else {
+        let result = (-n).toString().split('');
+  return result.reverse().join('');
+    }
   
 }
